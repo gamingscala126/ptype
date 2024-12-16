03d29ce9077d91bfd542ddeab450e7fa76317fe8
gamingscala126 <167210625+gamingscala126@users.noreply.github.com>
2024-12-16 01:16:24 -0500

    Update evilTypingScript.js
    iop

--- scripts/evilTypingScript.js
+++ scripts/evilTypingScript.js
@@ -92,17 +92,12 @@ userInput.addEventListener("input", () => {
         const endTime = new Date();
         const timeTaken = (endTime - startTime) / 1000; // Time in seconds
         const wpm = Math.round((targetText.split(" ").length / timeTaken) * 60);
-        if(isNaN(wpm))
-        {
-          window.location.href = "dddd.html";
-          {
-            else{
-                      window.location.href = "evil_results.html";
-            }
+
         // Save results to sessionStorage
         sessionStorage.setItem("wpm", wpm);
         sessionStorage.setItem("time", timeTaken.toFixed(2));
         sessionStorage.setItem("errors", errors);
+            window.location.href = "evil_results.html";
     }
 });
 
